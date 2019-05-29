e4770a6c0c9da947c316b79cc81612c5d76ca86f
Sergey V. Krupov <sergey.krupov@gmail.com>
2019-05-29 16:44:58 +0500

    Initial Commit

---
+++ AwesomeStopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeStopwatch
+//
+//  Created by Sergey V. Krupov on 29/05/2019.
+//  Copyright © 2019 Sergey V. Krupov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
